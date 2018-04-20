27c8307a657455b6c1826f5b4fd698c1b9d67938
shirou <shirou.faw@gmail.com>
2018-04-20 23:09:56 +0900

    add path

--- files.go
+++ files.go
@@ -2,14 +2,22 @@ package goash
 
 import (
 	"io/ioutil"
+	"strings"
 )
 
 func listFiles(path string) func(string) []string {
 	return func(line string) []string {
 		names := make([]string, 0, 10)
+		path := "/usr/bin"
+		prefix := line
+		if len(line) > 0 {
+			path = currentDir
+		}
 		files, _ := ioutil.ReadDir(path)
 		for _, f := range files {
-			names = append(names, f.Name())
+			if len(prefix) == 0 || strings.HasPrefix(f.Name(), prefix) {
+				names = append(names, f.Name())
+			}
 		}
 		return names
 	}
